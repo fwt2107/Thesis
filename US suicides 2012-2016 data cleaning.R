1fbe074f2fdbe2908acbe91530b598efca483987
Felix Tran <fwt2107@cumc.columbia.edu>
2018-09-19 21:12:53 -0400

    Changed initial dataframe into a tibble. Described major steps for cleaning the dataset. Finished 1st task of transforming suppressed death values into NA's.

--- US suicides 2012-2016 data cleaning.R
+++ US suicides 2012-2016 data cleaning.R
@@ -1,12 +1,33 @@
 # This file cleans the dataset containing crude rates of suicide for all
 #  US counties for the time period 2012-2016
+# 
+# These are the major tasks for cleaning this dataset:
+#  1. Transforming suppressed values into NA's
+#  2. Removing supressed and unreliability notes from the data
+#  3. Creating variables to note which rates were suppressed/unreliable
+#  4. In conjunction with steps 1 - 3, also creating a new tibble dataframe
+#      with only vectors (no factors) for ease of use during analyses
 
 # Read in dataset
-usa_suicides <- read.delim(file = '2012-2016 US counties suicides.txt', header = T)
+usa_suicides <- as.tibble(
+  read.delim(file = './Raw Data/2012-2016 US counties suicides.txt', header = T))
 
-# When read, the dataframe contains an Remove empty column
+# When read, the dataframe contains an empty column for notes, so it's removed
 usa_suicides <- usa_suicides[ , -1]
 
 # Rename columns for ease of use
-colnames(usa_suicides) <- c('county', 'code', 'deaths', 'pop', 'rate', 
+colnames(usa_suicides) <- c('county', 'code', 'deaths', 'pop', 'crude_rate', 
                             'ci_low95', 'ci_up95', 'se')
+
+# 1. Transforming suppressed values into NA's 
+# Will be our deaths vector in the future tibble dataframe
+deaths_vector <- vector(mode = "numeric")
+
+for (i in 1:nrow(usa_suicides)) {
+  if (usa_suicides$deaths[i] == "Suppressed")
+    deaths_vector[i] <- NA
+  else
+    deaths_vector[i] <- as.numeric(usa_suicides$deaths[i])
+}
+
+
